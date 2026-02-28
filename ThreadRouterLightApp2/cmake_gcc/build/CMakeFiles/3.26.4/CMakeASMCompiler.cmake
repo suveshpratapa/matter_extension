3d2b8ce63c6daa0f10d41bc64e6c7931b555a455
Suvesh Pratapa <suvesh.pratapa@silabs.com>
2026-02-27 22:15:14 -0500

    Support for newlib-nano compatibility.
    * Updated submodule for SDK with the above change
    * Rebuilt ThreadRouterLightApp
    * ThreadRouterLightApp2 contains the above app but also built with OPENTHREAD_CONFIG_TMF_NETDATA_SERVICE_ENABLE:1,OPENTHREAD_CONFIG_BORDER_ROUTER_ENABLE:1, to represent a full "client-server" example device showing TimeSync feature.

---
+++ ThreadRouterLightApp2/cmake_gcc/build/CMakeFiles/3.26.4/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-gcc")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-ar")
+set(CMAKE_ASM_COMPILER_AR "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-gcc-ar")
+set(CMAKE_RANLIB "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-gcc-ranlib")
+set(CMAKE_ASM_COMPILER_RANLIB "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-gcc-ranlib")
+set(CMAKE_LINKER "/Users/supratap/.silabs/slt/installs/conan/p/gcc-a86af308f7c14b/p/bin/arm-none-eabi-gcc")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
---
+++ ThreadRouterLightApp2/cmake_gcc/build/CMakeFiles/3.26.4/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.3.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.3.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/supratap/silabs-stash/matter_extension/ThreadRouterLightApp2/cmake_gcc/toolchain.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "arm")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
